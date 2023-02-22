5359fa0d4f46b2cf412ee0d23bf211ca6c03ff36
Mehak Nayab <nayabmeh@corp.contoursoftware.net>
2023-02-22 17:30:06 +0500

    add |incomplete

--- consumingFakeApis.js
+++ consumingFakeApis.js
@@ -1,5 +1,6 @@
 var mainDiv = document.getElementById('main');
 var usersArray = []
+
 // on document load,fetch users
 document.addEventListener('DOMContentLoaded', fetchUsers())
 
@@ -18,19 +19,67 @@ function fetchUsers() {
 
 function setCardDetails(users) {
     usersArray = users
-    console.log(users)
     //we have array of users
-    users.forEach(user => {
+    usersArray.forEach(user => {
         addCard(user)
     });
     document.getElementById("load").classList.add('d-none')
 }
 
+function addContact() {
+    let details = getDetails()
+    if (details == 'error')
+        return
+
+    usersArray.push(details)
+    addCard(usersArray)
+}
+
+
+function getDetails() {
+    //get values from input fields
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+
+    if (!isValidName(name) || !isValidEmail(email) || !isValidNumber(number)) {
+        document.getElementById('error').style.display = "block"
+        document.getElementById('error').innerHTML = error
+        return 'error'
+    }
+    document.getElementById('error').style.display = "none"
+
+
+    document.getElementById('error').innerHTML = error
+    // var imageSource = document.getElementById('file').files[0];
+
+    // // radio button checked
+    // var isProfessional = document.getElementById('inlineRadio2').checked;
+
+    // //value based on radio button type
+    // if (isProfessional) {
+    //     type = "Professional"
+    //     btnClass = "btn-success"
+    // } else {
+    //     type = "Personal"
+    //     btnClass = "btn-primary"
+    // }
+
+    details = {
+        "name": name,
+        "email": email,
+        "phone": number
+    }
+
+    return details
+}
+
+
 function addCard(user) {
 
-    console.log(user.isDeleted)
-if(user.id == 0)
-return
+    //deleted user's id is set to 0
+    if (user.id == 0)
+        return
 
     //card design
     var cardDesign = "<div id=" + user.id + " class='card bg-light mt-3 mb-3'><div class='card-body'><div class='row'>" +
@@ -59,7 +108,7 @@ return
         ' <input type="text" class="form-control mt-1" value=' + user.firstName + ' id=editFName' + user.id + '>' +
         ' <input type="text" class="form-control mt-1" value=' + user.lastName + ' id=editLName' + user.id + '>' +
         ' <input type="text" class="form-control mt-1"  value=' + user.email + ' id=editEmail' + user.id + '>' +
-        ' <input type="tel" class="form-control mt-1"  value="'+user.phone+'" id=editNumber' + user.id + '>' +
+        ' <input type="tel" class="form-control mt-1"  value="' + user.phone + '" id=editNumber' + user.id + '>' +
         '</div>' +
         '<div class="modal-footer">' +
         '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
@@ -71,17 +120,19 @@ return
 
     //add card to main div
     mainDiv.innerHTML += cardDesign
+    //add edit modal to main div
     mainDiv.innerHTML += editModal
-
 }
 
 function editUser(id) {
 
+    //get modal values
     var fName = document.getElementById('editFName' + id).value
     var lName = document.getElementById('editLName' + id).value
     var email = document.getElementById('editEmail' + id).value
     var number = document.getElementById('editNumber' + id).value
 
+    //validation
     if (!isValidName(fName) || !isValidName(lName) || !isValidEmail(email) || !isValidNumber(number)) {
         document.getElementById('error').style.display = "block"
         document.getElementById('error').innerHTML = error
@@ -91,6 +142,7 @@ function editUser(id) {
     document.getElementById('error').style.display = "none"
     document.getElementById("load").classList.remove('d-none')
 
+    //edit API
     fetch('https://dummyjson.com/users/' + id, {
         method: 'PUT', /* or PATCH */
         headers: { 'Content-Type': 'application/json' },
@@ -106,11 +158,12 @@ function editUser(id) {
 }
 
 function update(data) {
+    //array starts from 0 but db rows starts from 1
     index = data.id - 1
     usersArray[index] = data
     mainDiv.innerHTML = ""
     setCardDetails(usersArray)
-   document.getElementById("load").classList.add('d-none')
+    document.getElementById("load").classList.add('d-none')
 }
 
 function deleteUser(id) {
@@ -127,8 +180,8 @@ function removeDiv(id) {
 
     index = id - 1
     data = usersArray[index]
-data.id=0
-usersArray[index]=data
+    data.id = 0
+    usersArray[index] = data
 
     document.getElementById(id).classList.add("d-none")
     document.getElementById("load").classList.add("d-none")
@@ -137,7 +190,7 @@ usersArray[index]=data
 isValidName = (name) => {
     //only alphabets
     reg = /^[a-zA-Z]*$/
-   
+
     if (reg.test(name))
         return true
 
